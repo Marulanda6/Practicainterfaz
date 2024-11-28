4477d4ed50dfde808b138d5c6a843a9f3b11e0ba
Marulanda6 <nahim.marulanda686@ist17dejulio.edu.ec>
2024-11-28 16:02:58 -0500

    Implementacion de la interfaz

---
+++ src/modelo/FiguraGeometrica.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author kirlok6
+ */
+public interface FiguraGeometrica {
+    
+public final double PI = 3.1416;
+
+//DECARAR METODOS SIN IMPLEMENTARLOS 
+public void area ();
+public double perimetro();
+        
+    
+    
+    
+    
+}
